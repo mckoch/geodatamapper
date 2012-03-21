54089d10b3c08a600a84865deeb006177daf2b90
emcekah@gmail.com <emcekah@gmail.com@751072e2-486f-dc95-cb3a-8a18abb999a8>
2012-03-21 10:20:39 +0000

    Short sample API part 1 / kurze Bespiel-API

---
+++ SAMPLE_API.php
@@ -0,0 +1,8 @@
+<?php
+
+if (!headers_sent()){header('Content-Type: text/html;charset=windows-1252');}
+
+require_once('application.ini.php');
+require_once(INCLUDEDIR . 'apidispatcher.inc.php');
+
+
